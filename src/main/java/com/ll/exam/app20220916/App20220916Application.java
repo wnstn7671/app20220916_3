e063380f9d85187cb6259420520f3874b4afd2a2
wnstn7671 <wnstn7671@naver.com>
2023-07-20 22:40:43 +0900

    세팅

---
+++ src/main/java/com/ll/exam/app20220916/App20220916Application.java
@@ -0,0 +1,13 @@
+package com.ll.exam.app20220916;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App20220916Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App20220916Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ll/exam/app20220916/App20220916ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.exam.app20220916;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class App202209162ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
